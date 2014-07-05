7d39b9dd5d003252af2d077558f10b9c37431f43
Nicholas "LB" Braden <LB@LB-Stuff.com>
2014-07-05 09:26:18 -0500

    Reformat /horsestats command help

--- src/main/java/me/bdubz4552/horsestats/commands/Horsestats.java
+++ src/main/java/me/bdubz4552/horsestats/commands/Horsestats.java
@@ -1,6 +1,6 @@
 package me.bdubz4552.horsestats.commands;
 
-import org.bukkit.ChatColor;
+import static org.bukkit.ChatColor.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,11 +11,23 @@ import me.bdubz4552.horsestats.HorseStatsMain;
 import me.bdubz4552.horsestats.Message;
 
 public class Horsestats extends HorseStatsCommand implements CommandExecutor {
-	static ChatColor ccg = ChatColor.GREEN;
-	static ChatColor ccy = ChatColor.YELLOW;
-	
-	static String[] help = {ccg + "========================", ccy + "HorseStats by 'bdubz4552'", ccg + "========================", ccy + "Stat Display", ccg + "Grab a lead and punch a horse to return a list of statistics:", ccg + "-MaxHealth -Health -Jump Height -Speed (Blocks per Second)", ccg + "-Can Breed -Is Adult -Owner", ccy + "Noteworthy Things:", ccg + "1) Horses will NOT take damage from the punch", ccg + "2) Speed and jump values are not infinitely precise.", ccy + "Horse Teleporting", ccg + "Grab an ender pearl and punch a horse to select it. The damage will be canceled, and the horse will be selected for teleporting. To teleport the horse, use '/htp' at the desired destination and the horse will teleport to you.", ccy + "To see HorseStats commands, use '/help horsestats'. If this does not work, contact an administrator."};
-	
+
+	static String[] help =
+	{ GREEN  + "========================"
+	, YELLOW + "HorseStats by 'bdubz4552'"
+	, GREEN  + "========================"
+	, YELLOW + "Stat Display"
+	, GREEN  + "Grab a lead and punch a horse to return a list of statistics:"
+	, GREEN  + "-MaxHealth -Health -Jump Height -Speed (Blocks per Second)"
+	, GREEN  + "-Can Breed -Is Adult -Owner"
+	, YELLOW + "Noteworthy Things:"
+	, GREEN  + "1) Horses will NOT take damage from the punch"
+	, GREEN  + "2) Speed and jump values are not infinitely precise."
+	, GREEN  + "Horse Teleporting"
+	, GREEN  + "Grab an ender pearl and punch a horse to select it. The damage will be canceled, and the horse will be selected for teleporting. To teleport the horse, use '/htp' at the desired destination and the horse will teleport to you."
+	, YELLOW + "To see HorseStats commands, use '/help horsestats'. If this does not work, contact an administrator."
+	};
+
 	public Horsestats(HorseStatsMain horseStatsMain) {
 		this.main = horseStatsMain;
 	}
