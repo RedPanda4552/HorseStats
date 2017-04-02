f0b1002dd2e0faf4f241c230db12f0579e116cd6
Brian <redpanda4552@gmail.com>
2017-04-02 00:37:12 -0400

    Remove white space

--- src/main/java/io/github/redpanda4552/HorseStats/commands/CommandHPerm.java
+++ src/main/java/io/github/redpanda4552/HorseStats/commands/CommandHPerm.java
@@ -43,7 +43,6 @@ public class CommandHPerm extends AbstractCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        
         if (main.anarchyMode) {
             sender.sendMessage(lang.tag + lang.r + lang.get("hperm.the") + lang.y + " hperm " + lang.r + lang.get("hperm.anarchy-mode"));
             return true;
---
+++ src/main/java/io/github/redpanda4552/HorseStats/commands/CommandStatDB.java
@@ -0,0 +1,20 @@
+package io.github.redpanda4552.HorseStats.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import io.github.redpanda4552.HorseStats.Main;
+
+public class CommandStatDB extends AbstractCommand {
+
+    public CommandStatDB(Main main) {
+        super(main);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        return true;
+    }
+
+}
